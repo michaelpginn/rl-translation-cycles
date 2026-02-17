c7005def996816d2b3289472685fcfca4f2510ab
Michael Ginn <michaelginn00@gmail.com>
2026-02-16 17:33:52 -0700

    Update sbatch

--- scripts/run_single_gpu.sh
+++ scripts/run_single_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:h100_7g.80
 #SBATCH --ntasks=20
-#SBATCH --mem=64G
+#SBATCH --mem-per-cpu=4000m
 #SBATCH --time=2-00:00:00
 #SBATCH --output=logs/%j.log
 #SBATCH --job-name=rltc
